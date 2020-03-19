340a44744c9c19c3ad468997d5299d2fcb96d53b
javierbustillo <javier.bustillo@upr.edu>
2020-03-19 18:22:00 -0400

    Sigh

--- src/main/java/SAWC.java
+++ src/main/java/SAWC.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+
 public class SAWC implements StopAndWaitClient {
+    UDP udpClient;
+    int index;
+    int timeOut;
+    String divider;
+
+    public SAWC(String address, int portNumber) throws IOException {
+        this.udpClient = new UDP(address, portNumber);
+        this.index = 0;
+        this.timeOut = 3;
+    }
+
     @Override
     public boolean sendMessage(String data) {
+        boolean replied = false;
+        String frame = this.index + this.divider + data;
+        while (!replied) {
+            this.udpClient.sendMessage(frame);
+
+        }
+
         return false;
     }
 }
