13e34c1b1c7da846ff54c61ab57ad8dc1dda0091
javierbustillo <javier.bustillo@upr.edu>
2020-03-19 19:34:45 -0400

    Multithread

--- src/main/java/SAWC.java
+++ src/main/java/SAWC.java
@@ -1,26 +1,77 @@
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
+
+
+class Waiter implements Callable<String> {
+
+    UDP client;
+    public Waiter(UDP client){
+        this.client = client;
+    }
+
+
+    @Override
+    public String call() throws Exception {
+        return this.client.receiveMessage();
+    }
+}
+
 
 public class SAWC implements StopAndWaitClient {
     UDP udpClient;
     int index;
     int timeOut;
+    int timeOutLimit;
     String divider;
 
     public SAWC(String address, int portNumber) throws IOException {
         this.udpClient = new UDP(address, portNumber);
         this.index = 0;
-        this.timeOut = 3;
+        this.timeOut = 1;
+        this.timeOutLimit = 20;
     }
 
     @Override
     public boolean sendMessage(String data) {
         boolean replied = false;
         String frame = this.index + this.divider + data;
+
         while (!replied) {
             this.udpClient.sendMessage(frame);
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Future<String> future = executor.submit(new Waiter(this.udpClient));
+            String ack = null;
+            try{
+                ack = future.get(1, TimeUnit.SECONDS);
+            }
+            catch (TimeoutException e){
+                future.cancel(true);
+                this.timeOutLimit--;
+                System.out.println("Timeout sending again...");
+                if (this.timeOutLimit == 0){
+                    return false;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                executor.shutdownNow();
+            }
 
+            if (ack != null && ack.equals(Integer.toString(this.index))){
+                System.out.println("ACK Received");
+                replied = true;
+                this.index++;
+            }
         }
 
-        return false;
+        return true;
+    }
+
+    public void closeConnection() throws IOException {
+        this.udpClient.closeConnection();
     }
 }
